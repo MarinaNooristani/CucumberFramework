d17a254d093ca40bb252cb13afb8884fcb9d282d
MarinaNooristani <marina.nooristani321@gmail.com>
2022-04-30 14:04:30 +0300

    cucumber class 4

---
+++ src/test/java/utils/Constants.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class Constants {
+
+    public static final String CONFIGURATION_FILEPATH = System.getProperty("user.dir") + "/src/test/resources/config/config.properties";
+    //final--> unique
+    //CONFIGURATION_FILEPATH it's a good approach to write final variables in uppercase
+    public static final int IMPLICIT_WAIT = 10;
+    public static final int EXPLICIT_WAIT = 20;
+}
